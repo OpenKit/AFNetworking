e4c9f051029734e64055615f7304fab829e653dc
Suneet Shah <suneets@gmail.com>
2013-03-08 14:08:36 -0800

    created static library project

---
+++ AFNetworkingStatic/AFNetworkingStatic/AFNetworkingStatic.m
@@ -0,0 +1,13 @@
+//
+//  AFNetworkingStatic.m
+//  AFNetworkingStatic
+//
+//  Created by Suneet Shah on 3/8/13.
+//  Copyright (c) 2013 OpenKit. All rights reserved.
+//
+
+#import "AFNetworkingStatic.h"
+
+@implementation AFNetworkingStatic
+
+@end
